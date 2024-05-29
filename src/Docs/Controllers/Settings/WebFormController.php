e069b4206986434cdcda0e452ca465425fea1dc6
suraj kashyap <suraj.kashyap370@webkul.in>
2024-05-29 09:48:12 +0530

    write docs for webform.

--- src/Docs/Controllers/Settings/WebFormController.php
+++ src/Docs/Controllers/Settings/WebFormController.php
@@ -79,7 +79,115 @@ class WebFormController
      *      security={ {"sanctum_admin": {} }},
      *      @OA\RequestBody(
      *          required=true,
-     *          @OA\JsonContent(ref="#/components/schemas/WebForm")
+     *          @OA\JsonContent(
+     *            
+     *                @OA\Property(
+     *                    property="title",
+     *                    type="string",
+     *                    description="Web form title",
+     *                    example="Web form title"
+     *                ),
+     *                @OA\Property(
+     *                    property="description",
+     *                    type="string",
+     *                    description="Web form description",
+     *                    example="webform description"
+     *                ),
+     *                @OA\Property(
+     *                    property="submit_button_label",
+     *                    type="string",
+     *                    description="Label for the submit button",
+     *                    example="Submit Now"
+     *                ),
+     *                @OA\Property(
+     *                    property="submit_success_action",
+     *                    type="string",
+     *                    description="Action to take upon successful submission",
+     *                    example="message"
+     *                ),
+     *                @OA\Property(
+     *                    property="submit_success_content",
+     *                    type="string",
+     *                    description="Content to show upon successful submission",
+     *                    example="This is sample test message"
+     *                ),
+     *                @OA\Property(
+     *                    property="create_lead",
+     *                    type="string",
+     *                    description="Create lead option",
+     *                    example="on"
+     *                ),
+     *                @OA\Property(
+     *                    property="background_color",
+     *                    type="string",
+     *                    description="Background color",
+     *                    example="#F7F8F9"
+     *                ),
+     *                @OA\Property(
+     *                    property="form_background_color",
+     *                    type="string",
+     *                    description="Form background color",
+     *                    example="#FFFFFF"
+     *                ),
+     *                @OA\Property(
+     *                    property="form_title_color",
+     *                    type="string",
+     *                    description="Form title color",
+     *                    example="#263238"
+     *                ),
+     *                @OA\Property(
+     *                    property="form_submit_button_color",
+     *                    type="string",
+     *                    description="Form submit button color",
+     *                    example="#0E90D9"
+     *                ),
+     *                @OA\Property(
+     *                    property="attribute_label_color",
+     *                    type="string",
+     *                    description="Attribute label color",
+     *                    example="#546E7A"
+     *                ),
+     *                @OA\Property(
+     *                    property="attributes",
+     *                    type="object",
+     *                    @OA\Property(
+     *                        property="attribute_0",
+     *                        type="object",
+     *                        @OA\Property(
+     *                            property="attribute_id",
+     *                            type="string",
+     *                            description="ID of the attribute",
+     *                            example="9"
+     *                        ),
+     *                        @OA\Property(
+     *                            property="name",
+     *                            type="string",
+     *                            description="Name of the attribute",
+     *                            example="Name"
+     *                        ),
+     *                        @OA\Property(
+     *                            property="sort_order",
+     *                            type="integer",
+     *                            description="Sort order of the attribute",
+     *                            example=1
+     *                        ),
+     *                        @OA\Property(
+     *                            property="placeholder",
+     *                            type="string",
+     *                            nullable=true,
+     *                            description="Placeholder for the attribute",
+     *                            example=null
+     *                        ),
+     *                        @OA\Property(
+     *                            property="is_required",
+     *                            type="boolean",
+     *                            description="Is the attribute required",
+     *                            example=true
+     *                        )
+     *                    ),
+     *                    description="Attributes of the web form"
+     *                )
+     *          )
      *      ),
      *      @OA\Response(
      *          response=200,
@@ -119,9 +227,117 @@ class WebFormController
      *              format="int64"
      *          )
      *      ),
-     *      @OA\RequestBody(
+  *      @OA\RequestBody(
      *          required=true,
-     *          @OA\JsonContent(ref="#/components/schemas/WebForm")
+     *          @OA\JsonContent(
+     *            
+     *                @OA\Property(
+     *                    property="title",
+     *                    type="string",
+     *                    description="Web form title",
+     *                    example="Web form title"
+     *                ),
+     *                @OA\Property(
+     *                    property="description",
+     *                    type="string",
+     *                    description="Web form description",
+     *                    example="webform description"
+     *                ),
+     *                @OA\Property(
+     *                    property="submit_button_label",
+     *                    type="string",
+     *                    description="Label for the submit button",
+     *                    example="Submit Now"
+     *                ),
+     *                @OA\Property(
+     *                    property="submit_success_action",
+     *                    type="string",
+     *                    description="Action to take upon successful submission",
+     *                    example="message"
+     *                ),
+     *                @OA\Property(
+     *                    property="submit_success_content",
+     *                    type="string",
+     *                    description="Content to show upon successful submission",
+     *                    example="This is sample test message"
+     *                ),
+     *                @OA\Property(
+     *                    property="create_lead",
+     *                    type="string",
+     *                    description="Create lead option",
+     *                    example="on"
+     *                ),
+     *                @OA\Property(
+     *                    property="background_color",
+     *                    type="string",
+     *                    description="Background color",
+     *                    example="#F7F8F9"
+     *                ),
+     *                @OA\Property(
+     *                    property="form_background_color",
+     *                    type="string",
+     *                    description="Form background color",
+     *                    example="#FFFFFF"
+     *                ),
+     *                @OA\Property(
+     *                    property="form_title_color",
+     *                    type="string",
+     *                    description="Form title color",
+     *                    example="#263238"
+     *                ),
+     *                @OA\Property(
+     *                    property="form_submit_button_color",
+     *                    type="string",
+     *                    description="Form submit button color",
+     *                    example="#0E90D9"
+     *                ),
+     *                @OA\Property(
+     *                    property="attribute_label_color",
+     *                    type="string",
+     *                    description="Attribute label color",
+     *                    example="#546E7A"
+     *                ),
+     *                @OA\Property(
+     *                    property="attributes",
+     *                    type="object",
+     *                    @OA\Property(
+     *                        property="attribute_0",
+     *                        type="object",
+     *                        @OA\Property(
+     *                            property="attribute_id",
+     *                            type="string",
+     *                            description="ID of the attribute",
+     *                            example="9"
+     *                        ),
+     *                        @OA\Property(
+     *                            property="name",
+     *                            type="string",
+     *                            description="Name of the attribute",
+     *                            example="Name"
+     *                        ),
+     *                        @OA\Property(
+     *                            property="sort_order",
+     *                            type="integer",
+     *                            description="Sort order of the attribute",
+     *                            example=1
+     *                        ),
+     *                        @OA\Property(
+     *                            property="placeholder",
+     *                            type="string",
+     *                            nullable=true,
+     *                            description="Placeholder for the attribute",
+     *                            example=null
+     *                        ),
+     *                        @OA\Property(
+     *                            property="is_required",
+     *                            type="boolean",
+     *                            description="Is the attribute required",
+     *                            example=true
+     *                        )
+     *                    ),
+     *                    description="Attributes of the web form"
+     *                )
+     *          )
      *      ),
      *      @OA\Response(
      *          response=200,
