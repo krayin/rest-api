0e721231197c8edab6c8020a18449bec6b904b2a
suraj kashyap <suraj.kashyap370@webkul.in>
2024-06-25 13:07:16 +0530

    Minor changes.

--- src/Http/Controllers/V1/User/AccountController.php
+++ src/Http/Controllers/V1/User/AccountController.php
@@ -39,7 +39,8 @@ class AccountController extends Controller
             'email'            => 'email|unique:users,email,'.$user->id,
             'password'         => 'nullable|min:6|confirmed',
             'current_password' => 'nullable|required|min:6',
-            'image'            => 'mimes:jpeg,jpg,png,gif'
+            'image'            => 'mimes:jpeg,jpg,png,gif',
+            'remove_image'     => 'sometimes|boolean',
         ]);
 
         if (! Hash::check($data['current_password'], $user->password)) {
@@ -98,7 +99,7 @@ class AccountController extends Controller
     
         if (
             isset($data['remove_image'])
-            && $data['remove_image'] !== ''
+            && $data['remove_image']
         ) {
             $data['image'] = null;
         }
